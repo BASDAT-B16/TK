1786cd135f8186e1af65f57ebfead2d526e6c9bf
raisadndr <raisa100000@gmail.com>
2024-05-17 14:57:14 +0700

    added views.py for daftarkontributor

--- daftarkontributor/views.py
+++ daftarkontributor/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from psycopg2 import sql
+from pacilflix.database_manager import DatabaseManager
+
+from django.shortcuts import render
+from django.db import connection
 
 def show_daftarkontributor(request):
-    return render(request, "daftarkontributor.html")
+    kontributor = []
+    try:
+        tipe = request.GET.get('tipe', 'all')
+        print(tipe)
+        cursor = DatabaseManager.get_dict_cursor()
+        kontributor = cursor.fetchall()
+        cursor.close()
+    except Exception as e:
+        DatabaseManager.rollback()
+        print(e)
+    return render(request, "daftarkontributor.html", {'kontributor': kontributor})
