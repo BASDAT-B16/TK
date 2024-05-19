7ec6657a7428a63545cf2f2cfc1c8a0f73f901ea
fauzanjaisyu <fauzanjaisyu@gmail.com>
2024-05-19 13:15:49 +0700

    [REFACTOR] add SimpleConnectionPool to manage connection

--- pacilflix/database_manager.py
+++ pacilflix/database_manager.py
@@ -1,33 +1,66 @@
-# Database Connection Manager
-
 import psycopg2
 import psycopg2.extras
+import psycopg2.pool
 import environ
+import logging
 
-# Reference : https://www.psycopg.org/docs/usage.html
+# Set up basic logging
+logging.basicConfig(level=logging.INFO)
 
 env = environ.Env()
 environ.Env.read_env('.env')
 
-
 class DatabaseManager:
-    connection = psycopg2.connect(
-        user=env('DB_USER'),
-        password=env('DB_PASS'),
-        host=env('DB_HOST'),
-        port=env('DB_PORT'),
-        dbname=env('DB_NAME')
-    )
+    _connection_pool = None
 
+    @staticmethod
+    def initialize_connection_pool(minconn=1, maxconn=10):
+        if DatabaseManager._connection_pool is None:
+            DatabaseManager._connection_pool = psycopg2.pool.SimpleConnectionPool(
+                minconn,
+                maxconn,
+                user=env('DB_USER'),
+                password=env('DB_PASS'),
+                host=env('DB_HOST'),
+                port=env('DB_PORT'),
+                dbname=env('DB_NAME')
+            )
+            logging.info("Connection pool created")
+
+    @staticmethod
+    def get_connection():
+        if DatabaseManager._connection_pool is None:
+            DatabaseManager.initialize_connection_pool()
+        try:
+            return DatabaseManager._connection_pool.getconn()
+        except Exception as e:
+            logging.error(f"Error getting connection: {e}")
+            raise
+
+    @staticmethod
     def get_cursor():
-        print('get_cursor')
-        return DatabaseManager.connection.cursor()
+        conn = DatabaseManager.get_connection()
+        return conn.cursor()
 
+    @staticmethod
     def get_dict_cursor():
-        return DatabaseManager.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        conn = DatabaseManager.get_connection()
+        return conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
+    @staticmethod
     def commit():
-        DatabaseManager.connection.commit()
+        conn = DatabaseManager.get_connection()
+        conn.commit()
+        DatabaseManager._connection_pool.putconn(conn)
 
+    @staticmethod
     def rollback():
-        DatabaseManager.connection.rollback()
+        conn = DatabaseManager.get_connection()
+        conn.rollback()
+        DatabaseManager._connection_pool.putconn(conn)
+
+    @staticmethod
+    def close_all_connections():
+        if DatabaseManager._connection_pool:
+            DatabaseManager._connection_pool.closeall()
+            logging.info("All database connections closed")
